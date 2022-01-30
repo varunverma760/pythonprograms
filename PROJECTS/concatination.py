5593bb10cf6b95cd4c63194bc5e7559b32517131
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2022-01-30 15:37:35 +0530

    Create concatination.py

---
+++ PROJECTS/concatination.py
@@ -0,0 +1,13 @@
+#concatenation
+# youtuber = " varun verma" #some string concatenation
+# print("subscribe to "+ youtuber)
+# print("subscribe to {}" .format(youtuber))
+# print(f"subscriber to {youtuber}")
+
+adj= input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+madlib = f"Computer is so {adj}! it makes me so excited all the time because \
+    I like to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+print(madlib)
